379e4fcb04ac4240fc41e3df635b20699273ceab
Oleg-Lucenko <lucenko5577@gmail.com>
2023-04-20 15:18:08 +0300

    HW-27

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@
 import './App.css';
 import Header from './components/Header/Header.js';
 import Nav from './components/Nav/Nav.js';
-import Main from './components/Main/Main.js';
+
+
+import {HashRouter, Routes, Route, Link} from 'react-router-dom';
+
+
+import Home from './pages/Home.js';
+import Nervous from './pages/Nervous.js';
+import Respiratory from './pages/Respiratory.js';
+import Endocrine from './pages/Endocrine.js';
+import Cardiovascular from './pages/Cardiovascular.js';
+import Lymphatic from './pages/Lymphatic.js';
 
 function App() {
   return (
-    <div className="App">
 
-    <Header />
-    <Nav />
-    <Main />
+    <HashRouter>
+        <div className="App">
 
-    </div>
+          <Header />
+          <Nav />
+          <main>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/nervous.js" element={<Nervous />} />
+                <Route exact path="/respiratory.js" element={<Respiratory />} />
+                <Route exact path="/endocrine.js" element={<Endocrine />} />
+                <Route exact path="/cardiovascular.js" element={<Cardiovascular />} />
+                <Route exact path="/lymphatic.js" element={<Lymphatic />} />
+            </Routes>
+          </main>
+
+        </div>
+        </HashRouter>
+
+
   );
 }
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,6 @@
 
 import './Nav.css';
+import {Link} from 'react-router-dom';
 
 
 
@@ -7,11 +8,12 @@ import './Nav.css';
 function Nav() {
     return(
         <ul className="nav">
-             <li className="nav__item">
+             {/* <li className="nav__item">
                 <a href="/" className="nav__link">Home page</a>
             </li>
             <li className="nav__item">
                 <a href="/nervous.js" className="nav__link">Nervous system</a>
+                <Link to="/nervous.js" className="nav__link">Nervous system</Link>
             </li>
             <li className="nav__item">
                 <a href="/respiratory.js" className="nav__link">Respiratory system</a>
@@ -24,7 +26,26 @@ function Nav() {
             </li>
             <li className="nav__item">
                 <a href="/lymphatic.js" className="nav__link">Lymphatic system</a>
+            </li> */}
+            
+            <li className="nav__item">
+            <Link to="/" className="nav__link">Home page</Link>
             </li>
+            <li className="nav__item">
+             <Link to="/nervous.js" className="nav__link">Nervous system</Link>
+            </li>
+                <li className="nav__item">
+                    <Link to="/respiratory.js" className="nav__link">Respiratory system</Link>
+                </li>
+                <li className="nav__item">
+                    <Link to="/cardiovascular.js" className="nav__link">Cardiovascular system</Link>
+                </li>
+                <li className="nav__item">
+                    <Link to="/endocrine.js" className="nav__link">Endocrine system</Link>
+                </li>
+                <li className="nav__item">
+                    <Link to="/lymphatic.js" className="nav__link">Lymphatic system</Link>
+                </li>
 
         </ul>
 
